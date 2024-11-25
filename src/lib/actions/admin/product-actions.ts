95c4bb7b5036fb7a9419954079a2420ca906b849
Jupiter Moon <albinbtg@gmail.com>
2024-11-25 08:04:16 +0530

    server action to insert product

--- src/lib/actions/admin/product-actions.ts
+++ src/lib/actions/admin/product-actions.ts
@@ -5,12 +5,21 @@ import { checkIsAdmin } from "../auth-actions";
 import { parseProductData } from "@/lib/helpers/data-validation";
 import { findProductByName, insertProduct } from "@/lib/db-services/products";
 import { NOT_ADMIN_ERR_MESSAGE } from "../constants";
+import { db } from "@/drizzle/db";
+import {
+  productCategories,
+  products,
+  productVariantImages,
+  productVariants,
+} from "@/drizzle/schema";
 
 // server action to create a new product on admin req
 
 // todo - authenticate to protect server action
 
-export const createProduct = async (product: TProductData) => {
+export const createProductWithVariantsAndCategory = async (
+  product: TProductData,
+) => {
   try {
     // create a resp obj
     const response = { success: false, message: "" }; // check is admin
@@ -42,12 +51,57 @@ export const createProduct = async (product: TProductData) => {
 
     // insert the data to db if all checks are done
 
-    await insertProduct(parsedProduct);
+    await db.transaction(async (tx) => {
+      const {
+        productVariants: ProductVariantsInfo,
+        categories,
+        ...productInfo
+      } = parsedProduct;
 
-    // if insert success end success repsonse
+      // insert the new product info
 
-    response.success = true;
-    response.message = "Successfully created new category";
+      const newProduct = await tx
+        .insert(products)
+        .values(productInfo)
+        .returning({ productId: products.productId });
+
+      const productId = newProduct[0].productId;
+
+      // map categories-id and productId to insert into the product categories table
+
+      const categoryInsert = categories.map((categoryId) => {
+        return { productId, categoryId };
+      });
+
+      // insert productCatgories
+
+      await tx.insert(productCategories).values(categoryInsert);
+
+      ProductVariantsInfo.forEach(async (val) => {
+        const { productVariantImages: variantImages, ...variantInfo } = val;
+
+        const variantInsert = { ...variantInfo, productId };
+
+        const newVariant = await tx
+          .insert(productVariants)
+          .values(variantInsert)
+          .returning({ productVariantId: productVariants.productVariantId });
+
+        const imagesInsert = variantImages.map((imgUrl) => {
+          return { imgUrl, productVariantId: newVariant[0].productVariantId };
+        });
+
+        await tx.insert(productVariantImages).values(imagesInsert);
+      });
+
+      // if insert success end success repsonse
+
+      response.success = true;
+      response.message = "Successfully created new category";
+      return response;
+    });
+
+    response.message = "Unexpected Error Failed to Insert Product Try Again";
 
     return response;
   } catch (error: any) {
