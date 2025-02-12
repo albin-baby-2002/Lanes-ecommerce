53ca2693573c4fe36a77eab93b10e100b8a6df74
Jupiter Moon <albinbtg@gmail.com>
2025-02-12 08:31:03 +0530

    bug fix

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -3,7 +3,6 @@ import ProductsView from "@/sections/admin/products/views/products-view";
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const ProductsPage = async () => {
-  await delay(10000);
   return <ProductsView />;
 };
 
